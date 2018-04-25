389b07764ac9973140220d799cff6e313225d08f
Christopher J Hunter <cjhunter421@gmail.com>
2018-04-25 08:58:21 -0400

    Add files via upload

--- LoanCalc.js
+++ LoanCalc.js
@@ -2,7 +2,8 @@
 //inputs
 var principle = 20000;
 var rate = .05;
-var term = 15;
+var nper = 15;
+var paymentsPerYear = 12;
 var startDate = 1/1/2019;
 var extraPayment = 0;
 
@@ -10,6 +11,35 @@ var extraPayment = 0;
 var scheduledPayment;
 var payments = [];
 var paymentDates = [];
-var numScheduledPayments = term*12;
-var numActualPayments;
+var numScheduledPayments = nper*paymentsPerYear;
+var adjustedNumPayments;
 var interest;
+
+function pmt(rate,nper,pv) {
+  var pvif;
+  var pmt;
+
+  pvif = Math.pow(1+rate,nper);
+  pmt = rate/(pvif-1) * -(pv*pvif);
+
+  return pmt;
+};
+
+function paymentSchedule(principle,rate,paymentsPerYear,nper,pmt) {
+
+  var i = 0;
+  var schedule = [];
+  var balance = principle;
+  var numPayments = numScheduledPayments;
+    for (i=0;i<=numPayments; i++) {
+      var interestPayment = balance*(rate/100/paymentsPerYear);
+      var principlePayment = (pmt-interestPayment);
+      balance -= principlePayment;
+      var periodSummary = [i, principlePayment>0?(principlePayment<pmt?principlePayment:pmt):0, interestPayment>0?interestPayment:0, balance>0?balance:0]
+      schedule.push(periodSummary);
+    }
+    return schedule;
+
+
+
+};
