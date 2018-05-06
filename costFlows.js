73f3e033b48929eef7c5c4e4c410cb31f827cde8
Christopher J Hunter <cjhunter421@gmail.com>
2018-05-06 16:34:18 -0400

    Add files via upload

--- costFlows.js
+++ costFlows.js
@@ -1,9 +1,19 @@
 // generate projected cost flows
 var fs = require('fs');
 
-var data = fs.readFileSync("sfa1516.json"); 
+var data = JSON.parse(fs.readFileSync("sfaSmall.json")); 
 
-console.log(data[2]);
+nameMap = function(data){
+    map = [];
+    for (var i=0; i<data.length; i++){
+        map[data[i].SchoolName] = i;
+    };
+    return map;
+};
+var nameMap = nameMap(data);
+
+console.log(data[3101].SchoolName);
+console.log(nameMap["Massachusetts Institute of Technology"]);
 
 
 var mapping = [{"Bracket":"1","upper":"30000","lower":"0"},
@@ -18,7 +28,7 @@ getBracket = function(income){
     //bracket check descending
     //returns associated income quintile
     quint = 0;
-
+    //iterate downward through the possible income brackets until the correct quint is found
     for (i=4; i>=0; i--){
         if (income >= mapping[i].lower) {
             var quint = i+1;
@@ -29,21 +39,23 @@ getBracket = function(income){
 };
 
 getBracketVar = function(bracket){
-
+    //get the variable for the associated income bracket
     indexVar = incomeVars[bracket];
     return indexVar;
 };
 
-
-
-getNetSchoolCost = function(income,schoolName){
-
-    netVariable = getBracketVar(getBracket(income));
-    schoolNetCost = data[schoolName][netVariable];
+getNetSchoolCost = function(income,SchoolName){
+// find the relevant dataset field
+    var netVariable = getBracketVar(getBracket(income));
+//find the index of the school in the dataset
+    var schoolIndex = nameMap[SchoolName];
+//return the value in the field for that school
+    var schoolNetCost = data[schoolIndex][netVariable];
+    return schoolNetCost;
 
 };
-console.log(data[1]["Massachusetts Institute of Technology"]);
 
-//console.log(getNetSchoolCost(80000,"Massachusetts Institute of Technology"));
+
+console.log(getNetSchoolCost(80000,"Massachusetts Institute of Technology"));
 
 
